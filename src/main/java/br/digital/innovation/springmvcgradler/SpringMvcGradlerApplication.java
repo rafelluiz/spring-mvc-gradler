135b25fff8dd4ba295cdb71f71ce336f8fbf43f3
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-08-18 10:33:53 -0300

    heroku

---
+++ src/main/java/br/digital/innovation/springmvcgradler/SpringMvcGradlerApplication.java
@@ -0,0 +1,13 @@
+package br.digital.innovation.springmvcgradler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcGradlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcGradlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/digital/innovation/springmvcgradler/SpringMvcGradlerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.digital.innovation.springmvcgradler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcGradlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
